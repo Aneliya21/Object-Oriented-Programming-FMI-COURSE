351ac38ce17a3210e78fcb71bc5ef15670f8c91c
Aneliya21 <anelka.k.03@gmail.com>
2024-03-22 10:49:17 +0200

    Constants.h added

---
+++ Homeworks/task2-HTML_parser/Constants.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace Constants {
+    constexpr size_t FIELD_MAX_SIZE = 50; //after transf char entity ref
+    constexpr size_t MAX_FIELDS_ROW = 15;
+    constexpr size_t MAX_ROWS_SIZE = 100;
+
+    constexpr size_t FILE_TO_READ_LENGTH = 64;
+
+    const char TH_OPEN_TAG[] = "th";
+    const char TD_OPEN_TAG[] = "td";
+
+    const char TH_CLOSE_TAG[] = "/th";
+    const char TD_CLOSE_TAG[] = "/td";
+
+    const char SEPARATOR_LEFT = '<';
+    const char SEPARATOR_RIGHT = '>';
+
+    //COMMANDS
+    const char ADD[] = "add";
+    const char REMOVE[] = "remove";
+    const char EDIT[] = "edit";
+    const char PRINT[] = "print";
+    const char END[] = "end";
+
+}
