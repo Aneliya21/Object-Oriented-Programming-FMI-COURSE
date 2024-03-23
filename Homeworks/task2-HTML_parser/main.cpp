8fe7a13195aa2edeec23a77bef14077dfa0efff7
Aneliya21 <anelka.k.03@gmail.com>
2024-03-23 17:21:21 +0200

    main.cpp updated

--- Homeworks/task2-HTML_parser/main.cpp
+++ Homeworks/task2-HTML_parser/main.cpp
@@ -3,40 +3,96 @@
 
 int main() {
 
-    /*Row row1 = {"str1gsd", "str2", "str3"};
-    Row row2 = { "str1", "str2", "str3" };
-    //
-    Row row3 = { "str3", "str3", "str3" };
+    char fileName[Constants::FILE_TO_READ_LENGTH];
 
-    Row toAdd = { "fsad", "gre", "rsda", "tewfc"};
-
-    HTML_table t = { row1, row2, row3 };
-
-    t.colsCount = 3;
-    t.rowsCount = 3;
-
-    t.print();*/
-
-    char fileName[64];
+    std::cout << "file name to read table from: ";
 
     std::cin >> fileName;
+    std::cout << std::endl;
+
+    if (!fileName || strlen(fileName) > Constants::FILE_TO_READ_LENGTH) {
+        std::cout << "Invalid file name" << std::endl;
+        return -1;
+    }
 
     HTML_table t(fileName);
 
-    std::cout << "Commands: " << std::endl;
-    std::cout << '\t' << "> add" << std::endl;
-    std::cout << '\t' << "> remove" << std::endl;
-    std::cout << '\t' << "> edit" << std::endl;
-    std::cout << '\t' << "> print" << std::endl;
-    std::cout << '\t' << "> end" << std::endl;
+    AdditionalFunciotns::printCommands();
 
     char command[10];
 
     while (true) {
-        std::cout << "> ";
+        std::cout << std::endl << "> ";
         std::cin >> command;
 
+        if (!command || strlen(command) > 10) {
+            std::cout << "Invald command" << std::endl;
+            return -1;
+        }
+
         if (strcmp(command, Constants::ADD) == 0) {
+            std::cout << "add at index: ";
+            size_t index = 0;
+            std::cin >> index;
+
+            while (index<0 || index>t.getRowsCount()) {
+                std::cout << "Invalid index" << std::endl << "Try again" << std::endl;
+                std::cin >> index;
+                std::cout << std::endl;
+            }
+
+            Row toAdd;
+
+            std::cout << "fields count: ";
+            size_t count = 0;
+            std::cin >> count;
+            std::cout << std::endl;
+
+            while (count > t.getColsCount()) {
+                std::cout << "Invalid fields count" << std::endl << "Try again" << std::endl;
+                std::cin >> count;
+                std::cout << std::endl;
+            }
+
+            char field[Constants::FIELD_MAX_SIZE + 1];
+            field[Constants::FIELD_MAX_SIZE] = '\0';
+
+            char headerAns[4]; //4, because strlen(yes) = 3 + 1 for the '\0'
+            headerAns[3] = '\0';
+
+            for (size_t i = 0; i < count; i++) {
+                std::cout << "field name: ";
+                std::cin >> field;
+                std::cout << std::endl;
+
+                while (!field || strlen(field) > Constants::FIELD_MAX_SIZE) {
+                    std::cout << "Invalid field name" << std::endl << "Try again" << std::endl;
+                    std::cin >> field;
+                    std::cout << std::endl;
+                }
+
+                std::cout << "->is field header: ";
+                std::cin >> headerAns;
+                std::cout << std::endl;
+
+                if (strcmp(headerAns, "yes") == 0) {
+                    toAdd.headers[i] = true;
+                }
+                else if (strcmp(headerAns, "no") == 0) {
+                    toAdd.headers[i] = false;
+                }
+                else {
+                    while (strcmp(headerAns, "yes") != 0 || strcmp(headerAns, "no") != 0) {
+                        std::cout << "Invalid answer" << std::endl << "Try again" << std::endl;
+                        std::cin >> headerAns;
+                        std::cout << std::endl;
+                    }
+                }
+
+                strcpy(toAdd.fields[i], field);
+            }
+
+            t.add(index, toAdd);
 
         }
         else if (strcmp(command, Constants::REMOVE) == 0) {
@@ -61,8 +117,12 @@ int main() {
         else if (strcmp(command, Constants::PRINT) == 0) {
             t.print();
         }
-        else if (strcmp(command, "end") == 0) {
+        else if (strcmp(command, Constants::END) == 0) {
             break;
+        }
+        else {
+            std::cout << "Invalid command" << std::endl << "Try again: ";
+            continue;
         }
     }
     return 0;
