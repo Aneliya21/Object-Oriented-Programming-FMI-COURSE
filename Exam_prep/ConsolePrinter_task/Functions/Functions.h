1e319672deebc3c410538cbfc1965809c2567752
Aneliya21 <anelka.k.03@gmail.com>
2024-04-02 22:24:19 +0300

    Functions updated

---
+++ Exam_prep/ConsolePrinter_task/Functions/Functions.h
@@ -0,0 +1,11 @@
+namespace Funcs {
+	bool isDigit(char ch) {
+		return ch >= '0' && ch <= '9';
+	}
+	bool isLowerCase(char ch) {
+		return ch >= 'a' && ch <= 'z';
+	}
+	bool isUpperCase(char ch) {
+		return ch >= 'A' && ch <= 'Z';
+	}
+}
