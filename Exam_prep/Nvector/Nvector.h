168132874cf48f808622bc336a86ba4b7a67e6e4
Aneliya21 <anelka.k.03@gmail.com>
2024-04-03 21:17:39 +0300

    updated Nvector.h

--- Exam_prep/Nvector/Nvector.h
+++ Exam_prep/Nvector/Nvector.h
@@ -9,33 +9,25 @@ class Nvector {
 	void free();
 
 public:
-	Nvector() = default;
-
 	Nvector(size_t size);
 	Nvector(const Nvector& other);
 	Nvector& operator=(const Nvector& other);
 	~Nvector();
 
-	size_t getSize() const;
-
-	void setSize(size_t size);
-
-	int getElementFromVector(size_t index);
-
-	friend Nvector operator+(const Nvector& lhs, const Nvector& rhs);
-	friend Nvector operator-(const Nvector& lhs, const Nvector& rhs);
-
 	Nvector& operator+=(const Nvector& other);
 	Nvector& operator-=(const Nvector& other);
-
-	friend Nvector operator*(const Nvector& vector, size_t scalar);
 
 	Nvector& operator*=(size_t scalar);
 
 	friend std::ostream& operator<<(std::ostream& os, const Nvector& vect);
 	friend std::istream& operator>>(std::istream& is, Nvector& vect);
 
-	bool operator|=(const Nvector& other) const; //perpendicular
-	bool operator|(const Nvector& other) const; //parallel
+	int& operator[](size_t index);
+	int operator[](size_t index) const;
 
 };
+Nvector operator*(const Nvector& vector, size_t scalar);
+Nvector operator*(size_t scalar, const Nvector& vector);
+
+Nvector operator+(const Nvector& lhs, const Nvector& rhs);
+Nvector operator-(const Nvector& lhs, const Nvector& rhs);
